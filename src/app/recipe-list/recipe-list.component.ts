7749af3b974e4928a6120443f8ec7864417e68f2
AntonLebiodkin <anton.lebiodkin@gmail.com>
2016-10-10 23:36:23 +0300

    upd

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -7,9 +7,13 @@ import { Recipe } from '../shared/recipe';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent{
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe('Schnitzel', 'Very tasty', 'http://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-7_edited.jpg', ''),
+    new Recipe('Kholodets', 'Surprising dish', 'http://www.prelest.com/files/articles_big/holodec_1.jpg', ''),
+    new Recipe('Dummy', 'Weird Dummy', 'http://visioservice.ru/image/catalog/pokemon.png')
+  ];
   @Output() recipeSelected = new EventEmitter<Recipe>();
-  recipe = new Recipe('Dummy', 'Weird Dummy', 'http://visioservice.ru/image/catalog/pokemon.png');
+
 
   constructor() { }
 
